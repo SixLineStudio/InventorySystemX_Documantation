261287d19c00b380be049bfc4bfa637e21c317da
Michael <m19tes@gmail.com>
2023-04-30 20:11:01 +0300

    fsdgfsdsh sdsggsdg sdg sdg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,6 +16,7 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Shank', // Usually your GitHub or username.
