00c316819e8c8767a2983bc5c4f6cfc70924cc80
Michael <m19tes@gmail.com>
2023-05-01 16:52:35 +0300

    fsdgerheherherh  geg erge

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Inventory System X',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'A powerful inventory system for Unreal Engine',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
